4560c58a9b07b19491f43b5b01725c6392efafd2
Mitch <mjoerussell@gmail.com>
2021-07-19 21:58:24 -0500

    Fix line endings, update build.zig to use zig-odbc/build_pkg.zig

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const builtin = std.builtin;
+const build_odbc = @import("zig-odbc/build_pkg.zig");
 
 const test_files = .{ "src/main.zig", "src/connection.zig", "src/parameter.zig"};
 
@@ -14,17 +15,7 @@ pub fn build(b: *std.build.Builder) void {
     exe.setTarget(target);
     exe.install();
 
-    exe.addPackagePath("odbc", "zig-odbc/src/lib.zig");
-    exe.linkLibC();
-
-    const odbc_library_name = if (builtin.os.tag == .windows) "odbc32" else "odbc";
-    if (builtin.os.tag == .macos) {
-        exe.addIncludeDir("/usr/local/include");
-        exe.addIncludeDir("/usr/local/lib");
-    }
-    
-    // exe.linkSystemLibrary("c");
-    exe.linkSystemLibrary(odbc_library_name);
+    build_odbc.buildPkg(exe, "odbc");
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -35,6 +26,7 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    const odbc_library_name = if (builtin.os.tag == .windows) "odbc32" else "odbc";
     const test_step = b.step("test", "Run library tests");
     inline for (test_files) |filename| {
         var tests = b.addTest(filename);
