23ea3ec107e580cf5bf9d4931dc169e92431f674
Mitch <mjoerussell@gmail.com>
2022-04-13 15:24:32 -0500

    Add comments to connect_and_create_db

--- examples/src/connect_and_create_db.zig
+++ examples/src/connect_and_create_db.zig
@@ -6,6 +6,8 @@ const Connection = zdb.Connection;
 pub fn main() anyerror!void {
     const allocator = std.heap.page_allocator;
 
+    // In this example we'll create a new database and reconnect to it.
+    // The beginning is the same as basic_connect
     var basic_connect_config = Connection.ConnectionInfo.Config{
         .driver = "PostgreSQL Unicode(x64)",
         .database = "postgres",
@@ -31,9 +33,16 @@ pub fn main() anyerror!void {
         var cursor = try conn.getCursor(allocator);
         defer cursor.deinit(allocator) catch {};
 
+        // Once you have a cursor you can execute arbitrary SQL statements with executeDirect. executeDirect is a good option
+        // if you're only planning on executing a statement once. Parameters can be set with the final arg, but in this case
+        // none are required.
+        // Query results can be fetched using the ResultSet value returned by executeDirect. We don't care about the result set
+        // of this query, so we'll ignore it.
         _ = try cursor.executeDirect(allocator, "CREATE DATABASE create_example WITH OWNER = postgres", .{});
-    }
+    }   
 
+    // Now that the new DB was created we can connect to it. We'll use the same options as the original connection,
+    // except with the database field set to "create_example".
     basic_connect_config.database = "create_example";
     var example_connection_info = try Connection.ConnectionInfo.initWithConfig(allocator, basic_connect_config);
     defer example_connection_info.deinit();
@@ -42,11 +51,13 @@ pub fn main() anyerror!void {
     defer allocator.free(example_connection_string);
 
     {
+        // For now, we'll just connect and disconnect without doing anything.
         try conn.connectExtended(example_connection_string);
         defer conn.disconnect();
     }
 
     {
+        // Now we'll clean up the temp table
         try conn.connectExtended(pg_connection_string);
 
         var cursor = try conn.getCursor(allocator);
